aff66100d608534b5fedc0cc74fd91da8ccd6b52
William Tallarico <wct9667@rit.edu>
2025-01-25 14:04:55 -0500

    sorting based on priority

--- Impulse Control/Assets/Scripts/Emotions/EmotionSystem.cs
+++ Impulse Control/Assets/Scripts/Emotions/EmotionSystem.cs
@@ -1,3 +1,5 @@
+using System.Collections.Generic;
+using System.Linq;
 using ImpulseControl.Events;
 using UnityEngine;
 
@@ -16,10 +18,14 @@ namespace ImpulseControl
         [SerializeField] private Emotion fear;
         [SerializeField] private Emotion envy;
 
-        private int priorityA = 1;
-        private int priorityF = 2;
-        private int priorityE = 3;
+        private int angerPriority = 3;
+        private int fearPriority = 1;
+        private int envyPriority = 2;
 
+        private Dictionary<Emotion, int> emotionPriority;
+        private List<(Emotion emotion, System.Action crashAction)> emotions;
+            
+        
         public Emotion Anger => anger;
         public Emotion Fear => fear;
         public Emotion Envy => envy;
@@ -29,39 +35,64 @@ namespace ImpulseControl
             anger.Start();
             fear.Start();
             envy.Start();
+            
+            //list of emotions and their corresponding crash actions
+            emotions = new List<(Emotion emotion, System.Action crashAction)>()
+            {
+                (anger, () =>
+                {
+                    EventBus<Event_CrashOut>.Raise(new Event_CrashOut()
+                    {
+                        emotionType = anger.EmotionType
+                    });
+                    envy.CrashOut();
+                    fear.CrashOut();
+                }),
+                (fear, () =>
+                {
+                    EventBus<Event_CrashOut>.Raise(new Event_CrashOut()
+                    {
+                        emotionType = fear.EmotionType
+                    });
+                    envy.CrashOut();
+                    anger.CrashOut();
+                }),
+                (envy, () =>
+                {
+                    EventBus<Event_CrashOut>.Raise(new Event_CrashOut()
+                    {
+                        emotionType = envy.EmotionType
+                    });
+                    fear.CrashOut();
+                    anger.CrashOut();
+                })
+            };
+            //dictionary to help with sorting
+            emotionPriority = new Dictionary<Emotion, int>
+            {
+                { anger, angerPriority },
+                { fear, fearPriority },
+                { envy, envyPriority }
+            };
         }
 
+       
         private void Update()
         {
-            if (anger.Update())
+            //sort emotions by corresding priority
+            List<(Emotion emotion, System.Action crashAction)> emotionSorted
+                = emotions.OrderByDescending((e => emotionPriority[e.emotion])).ToList();
+
+            //check for crash and update each emotion
+            foreach ((Emotion emotion, System.Action crashAction) in emotionSorted)
             {
-                EventBus<Event_CrashOut>.Raise(new Event_CrashOut()
+                Debug.Log((emotion.EmotionType));
+                if (emotion.Update())
                 {
-                    emotionType = anger.EmotionType
-                });
-                envy.CrashOut();
-                fear.CrashOut();
-                return;
-            };
-            if (fear.Update())
-            {
-                EventBus<Event_CrashOut>.Raise(new Event_CrashOut()
-                {
-                    emotionType = fear.EmotionType
-                });
-                envy.CrashOut();
-                anger.CrashOut();
-                return;
-            };
-            if(envy.Update())
-            {
-                EventBus<Event_CrashOut>.Raise(new Event_CrashOut()
-                {
-                    emotionType = fear.EmotionType
-                });
-                fear.CrashOut();
-                anger.CrashOut();
-            };
+                    crashAction();
+                    break;
+                }
+            }
         }
     }
 }
