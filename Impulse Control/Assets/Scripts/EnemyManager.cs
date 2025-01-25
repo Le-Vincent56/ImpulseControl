04d63edc11bd71b7641dae60cad7b9193d14dab2
Frank Alfano <q24xboss@gmail.com>
2025-01-25 16:22:24 -0500

    Added enemy object pooling + spawning + scaling

---
+++ Impulse Control/Assets/Scripts/EnemyManager.cs
@@ -0,0 +1,140 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace ImpulseControl {
+	public class EnemyManager : MonoBehaviour {
+		[Header("References")]
+		[SerializeField] private GameObject normalEnemyObjectPrefab;
+		[SerializeField] private List<GameObject> normalEnemyObjectPool;
+		[SerializeField] private GameObject tankEnemyObjectPrefab;
+		[SerializeField] private List<GameObject> tankEnemyObjectPool;
+		[SerializeField] private GameObject rangedEnemyObjectPrefab;
+		[SerializeField] private List<GameObject> rangedEnemyObjectPool;
+		[Header("Properties")]
+		[SerializeField] private int currentWave;
+		[SerializeField] private int remainingEnemies;
+
+		/// <summary>
+		/// The current wave of the game
+		/// </summary>
+		public int CurrentWave { get => currentWave; private set => currentWave = value; }
+
+		/// <summary>
+		/// The number of enemies that remain in the scene
+		/// </summary>
+		public int RemainingEnemies {
+			get => remainingEnemies;
+			set {
+				remainingEnemies = value;
+
+				// If there are no more remaining enemies, spawn the next wave
+				if (remainingEnemies == 0) {
+					SpawnWaveEnemies( );
+				}
+			}
+		}
+
+		private void Awake ( ) {
+			normalEnemyObjectPool = new List<GameObject>( );
+			tankEnemyObjectPool = new List<GameObject>( );
+			rangedEnemyObjectPool = new List<GameObject>( );
+		}
+
+		public void SpawnWaveEnemies ( ) {
+			// https://www.desmos.com/calculator/4ibnli36yg
+
+			// Calculate the number of enemies to spawn for the current wave
+			float ec_a = 0.0208333f;
+			float ec_b = -0.9f;
+			float ec_c = 14.41667f;
+			float ec_d = 15f;
+			int enemyCount = Mathf.RoundToInt((ec_a * Mathf.Pow(CurrentWave, 3)) + (ec_b * Mathf.Pow(CurrentWave, 2)) + (ec_c * CurrentWave) + ec_d + CurrentWave);
+
+			for (int i = 0; i < enemyCount; i++) {
+				// Calculate the normal enemy's spawn chance
+				float ne_a = 0.914286f;
+				float ne_b = 0.870551f;
+				float ne_c = 0.0857143f;
+				float normalEnemyChance = ne_a * Mathf.Pow(ne_b, CurrentWave) + ne_c;
+
+				// Calculate the tank enemy's spawn chance
+				float te_a = 0.0000888889f;
+				float te_b = -0.00311111f;
+				float te_c = 0f;
+				float te_d = 1f;
+				float tankEnemyChance = (te_a * Mathf.Pow(CurrentWave, 3)) + (te_b * Mathf.Pow(CurrentWave, 2)) + (te_c * CurrentWave) + te_d;
+
+				// The remaining percent chance will be for ranged enemies
+
+				// Generate a random value and see which enemy spawns
+				float randomValue = Random.Range(0f, 1f);
+				if (randomValue < normalEnemyChance) {
+					SpawnNormalEnemy( );
+				} else if (randomValue < tankEnemyChance) {
+					SpawnTankEnemy( );
+				} else {
+					SpawnRangedEnemy( );
+				}
+			}
+
+			// Update the wave variables for the enemy spawner
+			CurrentWave++;
+			RemainingEnemies = enemyCount;
+		}
+
+		/// <summary>
+		/// Spawn a normal enemy, either from enabling in the object pool or instantiating a new object
+		/// </summary>
+		public void SpawnNormalEnemy ( ) {
+			// Loop through all enemies that are in the object pool
+			foreach (GameObject normalEnemy in normalEnemyObjectPool) {
+				// If the enemy is currently disabled, it can be "spawned in" again
+				if (!normalEnemy.activeInHierarchy) {
+					normalEnemy.SetActive(true);
+					return;
+				}
+			}
+
+			// If there were no available enemies in the pool to get, spawn a new one and add it to the list
+			GameObject newNormalEnemy = Instantiate(normalEnemyObjectPrefab);
+			normalEnemyObjectPool.Add(newNormalEnemy);
+		}
+
+		/// <summary>
+		/// Spawn a tank enemy, either from enabling in the object pool or instantiating a new object
+		/// </summary>
+		public void SpawnTankEnemy ( ) {
+			// Loop through all enemies that are in the object pool
+			foreach (GameObject tankEnemy in tankEnemyObjectPool) {
+				// If the enemy is currently disabled, it can be "spawned in" again
+				if (!tankEnemy.activeInHierarchy) {
+					tankEnemy.SetActive(true);
+					return;
+				}
+			}
+
+			// If there were no available enemies in the pool to get, spawn a new one and add it to the list
+			GameObject newTankEnemy = Instantiate(tankEnemyObjectPrefab);
+			tankEnemyObjectPool.Add(newTankEnemy);
+		}
+
+		/// <summary>
+		/// Spawn a ranged enemy, either from enabling in the object pool or instantiating a new object
+		/// </summary>
+		public void SpawnRangedEnemy ( ) {
+			// Loop through all enemies that are in the object pool
+			foreach (GameObject rangedEnemy in rangedEnemyObjectPool) {
+				// If the enemy is currently disabled, it can be "spawned in" again
+				if (!rangedEnemy.activeInHierarchy) {
+					rangedEnemy.SetActive(true);
+					return;
+				}
+			}
+
+			// If there were no available enemies in the pool to get, spawn a new one and add it to the list
+			GameObject newRangedEnemy = Instantiate(rangedEnemyObjectPrefab);
+			rangedEnemyObjectPool.Add(newRangedEnemy);
+		}
+	}
+}
