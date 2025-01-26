eb3239313eea5168d2de5c64c1704c81d7efe3e5
Frank Alfano <q24xboss@gmail.com>
2025-01-25 20:31:39 -0500

    Added skill tree unlocking and buying

--- Impulse Control/Assets/Scripts/SkillTree/SkillNode.cs
+++ Impulse Control/Assets/Scripts/SkillTree/SkillNode.cs
@@ -6,9 +6,10 @@ using UnityEngine;
 namespace ImpulseControl {
 	public class SkillNode : MonoBehaviour {
 		[Header("References")]
-		[SerializeField] private List<SkillNode> parentSkillNodes = new List<SkillNode>( );
 		[SerializeField] private List<SkillNode> childSkillNodes = new List<SkillNode>( );
+		[SerializeField] private SpriteRenderer spriteRenderer;
 		[Header("Properties")]
+		[SerializeField] private bool _isVisible;
 		[SerializeField] private bool _isUnlocked;
 		[SerializeField] private bool _isBought;
 		[SerializeField] private string _title;
@@ -20,32 +21,54 @@ namespace ImpulseControl {
 		[SerializeField] private PlayerModifiers _playerModifiers;
 
 		/// <summary>
+		/// Check to see if this skill node is visible on the skill tree
+		/// </summary>
+		public bool IsVisible {
+			get => _isVisible;
+			set {
+				_isVisible = value;
+				UpdateAlpha( );
+			}
+		}
+
+		/// <summary>
 		/// Check to see if this skill node is currently unlocked
 		/// </summary>
 		public bool IsUnlocked {
 			get => _isUnlocked;
 			set {
 				_isUnlocked = value;
+				UpdateAlpha( );
 			}
 		}
 
 		/// <summary>
 		/// Check to see if this skill node is currently already bought
 		/// </summary>
-		public bool IsBought { get => _isBought; private set => _isBought = value; }
+		public bool IsBought {
+			get => _isBought;
+			set {
+				_isBought = value;
+				UpdateAlpha( );
+			}
+		}
 
 		/// <summary>
 		/// The number of skill points that this skill node costs
 		/// </summary>
 		public int SkillPointCost => _skillPointCost;
 
+		/// <summary>
+		/// A list of all the child skill nodes of this skill node
+		/// </summary>
+		public List<SkillNode> ChildSkillNodes => childSkillNodes;
 
 		/// NOTE: Ignore the billion warnings that this on validate method throws, unity is just a crybaby
 		private void OnValidate ( ) {
 			// Round the skill nodes transform to be on a grid
 			transform.position = new Vector3(Mathf.RoundToInt(transform.position.x), Mathf.RoundToInt(transform.position.y));
 
-			for (int i = 0; i < childSkillNodes.Count; i++) {
+			for (int i = 0; i < ChildSkillNodes.Count; i++) {
 				// If there are no more node connections, then return from the loop
 				if (transform.childCount == i) {
 					Debug.LogWarning("There needs to be more node connections on skill node: " + name);
@@ -56,12 +79,10 @@ namespace ImpulseControl {
 				Transform nodeConnection = transform.GetChild(i);
 
 				// Get the direction from this skill node to the child skill node
-				Vector2 direction = childSkillNodes[i].transform.position - transform.position;
+				Vector2 direction = ChildSkillNodes[i].transform.position - transform.position;
 
-				// Set the size of the node connection
+				// Set the size, position, and rotation of the node connection
 				nodeConnection.GetComponent<SpriteRenderer>( ).size = new Vector2(0.25f, direction.magnitude);
-
-				// Set the position and rotation of the node connection
 				nodeConnection.localPosition = direction / 2f;
 				nodeConnection.localRotation = Quaternion.LookRotation(Vector3.back, direction);
 			}
@@ -71,25 +92,74 @@ namespace ImpulseControl {
 			OnValidate( );
 		}
 
+		private void Start ( ) {
+			// Update the alpha of this node
+			UpdateAlpha( );
+
+			// If this skill node is starting as bought, then update all child nodes as well
+			if (IsBought) {
+				UnlockChildNodes( );
+			}
+		}
+
 		private void OnMouseDown ( ) {
 			// If the skill node is not unlocked or has already been bought, then return and do nothing
 			if (!IsUnlocked || IsBought) {
 				return;
 			}
 
-			PlayerExperience playerExperience = FindObjectOfType<PlayerExperience>( );
+			// PlayerExperience playerExperience = FindObjectOfType<PlayerExperience>( );
 
 			// If the player does not have enough skill points to buy this skill node, then return and do nothing
-			if (playerExperience == null || playerExperience.SkillPoints < SkillPointCost) {
-				return;
-			}
+			// if (playerExperience == null || playerExperience.SkillPoints < SkillPointCost) {
+			// return;
+			// }
 
+			// playerExperience.SkillPoints -= SkillPointCost;
+			IsBought = true;
+			UnlockChildNodes( );
+		}
+
+		/// <summary>
+		/// Update and unlock child nodes when this skill node is bought
+		/// </summary>
+		private void UnlockChildNodes ( ) {
 			// Since this parent is being bought, unlock the child nodes as well
-			foreach (SkillNode childSkillNode in childSkillNodes) {
+			foreach (SkillNode childSkillNode in ChildSkillNodes) {
 				childSkillNode.IsUnlocked = true;
+
+				// Set all of the child nodes of that skill node to be visible but not unlocked
+				foreach (SkillNode subChildSkillNode in childSkillNode.ChildSkillNodes) {
+					subChildSkillNode.IsVisible = true;
+				}
+			}
+		}
+
+		/// <summary>
+		/// Update the alpha of both this node and the node connections based on this node's state
+		/// </summary>
+		private void UpdateAlpha ( ) {
+			// Get the node alpha value based on this node's state
+			float nodeAlpha = 0f;
+			if (IsUnlocked) {
+				nodeAlpha = 1f;
+			} else if (IsVisible) {
+				nodeAlpha = 0.1f;
 			}
 
-			playerExperience.SkillPoints -= SkillPointCost;
+			spriteRenderer.color = new Color(1f, 1f, 1f, nodeAlpha);
+
+			// Get the node connection alpha value based on this node's state
+			float nodeConnectionAlpha = 0f;
+			if (IsBought) {
+				nodeConnectionAlpha = 1f;
+			} else if (IsUnlocked) {
+				nodeConnectionAlpha = 0.1f;
+			}
+
+			for (int i = 0; i < transform.childCount; i++) {
+				transform.GetChild(i).GetComponent<SpriteRenderer>( ).color = new Color(1f, 1f, 1f, nodeConnectionAlpha);
+			}
 		}
 	}
 }
