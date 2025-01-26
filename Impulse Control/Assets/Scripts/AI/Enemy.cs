29a78a84518fe2376e4f0319ce67307a6d4a2fd9
JonathanBenz <98258254+JonathanBenz@users.noreply.github.com>
2025-01-26 15:49:48 -0500

    Player gets XP when enemy dies

--- Impulse Control/Assets/Scripts/AI/Enemy.cs
+++ Impulse Control/Assets/Scripts/AI/Enemy.cs
@@ -108,6 +108,7 @@ namespace ImpulseControl.AI
         }
         public void SendToObjectPool()
         {
+            player.GetComponent<PlayerExperience>().ExperiencePoints += Random.Range(1, 4);
             this.gameObject.SetActive(false);
         }
         protected void At(IState from, IState to, IPredicate condition) => stateMachine.AddTransition(from, to, condition);
